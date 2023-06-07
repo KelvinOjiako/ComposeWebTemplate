4ad708e46111c5f3246590a0b27988d9900aca37
kelvin Ojiako <51576899+KelvinOjiako@users.noreply.github.com>
2023-06-06 20:40:29 -0400

    composeHTML module edits

--- ComposeHTML/jsFrontEnd/src/jsMain/kotlin/Main.kt
+++ ComposeHTML/jsFrontEnd/src/jsMain/kotlin/Main.kt
@@ -1,9 +1,12 @@
 import com.pablichj.incubator.uistate3.BrowserViewportWindow
+import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.skiko.wasm.onWasmReady
 
 fun main(){
     onWasmReady {
         BrowserViewportWindow(){
+
+            Text("The js COmpose Magic is ON!!!")
 
         }
     }
